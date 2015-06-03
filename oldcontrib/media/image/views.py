8e728e60e3f70777efec93f9aa28b82dc9efd51f
Kasey Kelly <kasey@servee.com>
2015-06-03 15:15:39 -0400

    imports added

--- oldcontrib/media/image/views.py
+++ oldcontrib/media/image/views.py
@@ -1,37 +1,25 @@
+import json
+
 from django.http import HttpResponse
 from django.template.loader import render_to_string
-from django.utils import simplejson
+
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
 from django.contrib.auth.decorators import login_required
 
 from oldcontrib.media.image.forms import ImageUpload
-
 from oldcontrib.media.image.models import Image
 
 @csrf_exempt
 @require_POST
 @login_required
 def upload_image(request):
-    """
-    This view accepts 5 values (gallery, position, app_label, model, and pk) which are POSTed.
-    Checks the values and adds the item at the specified position.
-    """
-    ret = {}
-    ret['error'] = []
-
-    img = ImageUpload(request.POST, request.FILES)
-    if img.is_valid():
-        image = img.save()
-        ret['item'] = render_to_string('panels/add_image.html',dict(image=image))
-    else:
-        ret['error'].append('Not a valid form')
-        ret['error'].append(img.errors)
-
-    # return result
-    resp = simplejson.dumps(ret)
-    return HttpResponse(resp, mimetype='application/json')
+    images = []
+    for f in request.FILES.getlist("file"):
+        obj = Image.objects.create(image=f)
+        images.append({"filelink": obj.image.url})
+    return HttpResponse(json.dumps(images), mimetype="application/json")
 
 @login_required
 def recent_photos(request):
