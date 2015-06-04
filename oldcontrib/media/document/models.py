66310323a00aeb5d91ba217e22fb524cc301cf7f
Kasey Kelly <kasey@servee.com>
2015-06-04 10:47:56 -0400

    grr

--- oldcontrib/media/document/models.py
+++ oldcontrib/media/document/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 class Document(models.Model):
     """Document model"""
     title = models.CharField(max_length=255, blank=True, null=True)
-    document = models.FileField(upload_to="documents")
+    document = models.FileField(upload_to="servee_documents")
     description = models.TextField(blank=True, null=True)
     text = models.TextField(blank=True, null=True)
     uploaded = models.DateTimeField(auto_now_add=True)
@@ -13,7 +13,7 @@ class Document(models.Model):
 
     def __unicode__(self):
         return '%s' % self.title
-        
+
     def save(self, *args, **kwargs):
         if not self.title:
             self.title = self.document.name
--- oldcontrib/media/image/models.py
+++ oldcontrib/media/image/models.py
@@ -5,14 +5,14 @@ from django.conf import settings
 class Image(models.Model):
     """Image model"""
     title = models.CharField(max_length=255, blank=True, null=True)
-    image = models.ImageField(upload_to="images")
+    image = models.ImageField(upload_to="servee_images")
     description = models.TextField(blank=True, null=True)
     uploaded = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return '%s' % self.title
-        
+
     def save(self, *args, **kwargs):
         if not self.title:
             self.title = self.image.name
